1e1246e26bb8e7f29a01cd605783578cc94d0dc5
Brian Brewer <rewerb@hotmail.com>
2018-10-02 18:36:08 -0500

    initialCommit

---
+++ OdeToFood/OdeToFood/IGreeter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace OdeToFood
+{
+    public interface IGreeter
+    {
+        string GetMessageOfTheDay();
+    }
+
+    public class Greeter: IGreeter
+    {
+        IConfiguration configuration;
+
+        public Greeter(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public string GetMessageOfTheDay()
+        {
+            return configuration["Greeting"] + " via Greeter";
+        }
+    }
+}
---
+++ OdeToFood/OdeToFood/Startup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OdeToFood.Data;
+using OdeToFood.Services;
+
+namespace OdeToFood
+{
+    public class Startup
+    {
+        private IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IGreeter, Greeter>();
+            services.AddDbContext<OdeToFoodDbContext>(Options => Options.UseSqlServer(_configuration.GetConnectionString("OdeToFood")));
+            services.AddScoped<IRestaurantData, SqlRestaurantData>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, 
+                              IHostingEnvironment env,
+                              //IConfiguration configuration)
+                              IGreeter greeter)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+
+                //var greeting = configuration["Greeting"];//reads from cmd line then environment then appsettings
+                var greeting = greeter.GetMessageOfTheDay();
+                await context.Response.WriteAsync(greeting);
+            });
+        }
+    }
+}
